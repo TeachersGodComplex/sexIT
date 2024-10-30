a5a61b982ff8dd68a4096672afa64401b7a25b1f
TeachersGodComplex <ida@svahndata.se>
2024-10-30 16:08:15 +0100

    färdig att pusha

--- scripts/timer.js
+++ scripts/timer.js
@@ -2,10 +2,11 @@ import dayjs from "dayjs";
 
 export function updateTimers() {
   const timers = document.querySelectorAll(".timer > span");
-  const time = dayjs.duration(dayjs("2024-12-24T20:00:00.000Z").diff(dayjs()));
+  const time = dayjs.duration(dayjs("2024-11-26T20:00:00.000Z").diff(dayjs()));
   for (const el of timers) {
     el.textContent = time.format("DD:HH:mm:ss");
   }
 
   setTimeout(updateTimers, 1000);
 }
+
